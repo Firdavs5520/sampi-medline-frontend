0ab10540cff7e07459d3f07cc233a54744289078
Firdavs <sharipovfirdavs5520@gmail.com>
2026-02-03 22:25:36 +0500

    oxirgisi

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,20 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+// 🔥 SERVICE WORKER REGISTER
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(() => {
+        console.log("Service Worker registered");
+      })
+      .catch((err) => {
+        console.error("SW registration failed:", err);
+      });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
